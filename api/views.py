0340f568e7728e0fb25897dc35a8b7c68146c4a6
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-12 12:08:44 +0530

    Updated views.py
    Created student_list function to get data of all students.
    
    JsonResponse is used instead of JSONRenderer() and HttpResponse().

--- api/views.py
+++ api/views.py
@@ -4,12 +4,21 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
-#model object - single student data
+#model object - single student's data
 def student_detail(request, pk):
     stu = Student.objects.get(id=pk)  #complex data
     serializer = StudentSerializer(stu)  # in python data
+    #json_data = JSONRenderer().render(serializer.data) # to convert python data into json
+    #return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
+
+# query set - each student's data
+def student_list(request):
+    stu = Student.objects.all()  #complex data
+    serializer = StudentSerializer(stu, many=True)  # in python data
     json_data = JSONRenderer().render(serializer.data) # to convert python data into json
     return HttpResponse(json_data, content_type='application/json')
+    
