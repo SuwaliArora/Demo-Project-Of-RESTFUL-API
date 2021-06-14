564d43a71902f6781eefd9731e4f058fa9816ec5
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-15 00:01:00 +0530

    Created serializers.py

---
+++ NewProject2/api/serializers.py
@@ -0,0 +1,12 @@
+from NewProject2.api.models import Student
+from rest_framework import serializers
+from .models import Student
+
+class StudentSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=100)
+    roll = serializers.IntegerField()
+    city = serializers.CharField(max_length=100)
+
+    # create operation
+    def create(self, validated_data):
+        return Student.objects.create(**validated_data)
