201a9798a38b97cd7f684c0933e2ad2d6f585175
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-15 23:29:40 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -1,25 +1,24 @@
-import json
-import re
+from rest_framework import serializers
+from api.models import Student
 from django.shortcuts import render
-import io
-from rest_framework.parsers import JSONParser
+from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
 # Create your views here.
-def student_create(request):
-    if request.method == 'POST':
-        json_data = request.body
-        stream = io.BytesIO(json_data)
-        pythondata = JSONParser().parse(stream)
-        #to convert python data into complex data
-        serializer = StudentSerializer(pythondata)
-        if serializer.is_valid():
-            serializer.save()
-            res = {'msg': 'Data created'}
-            json_data = JSONRenderer().render(msg)
-            return HttpResponse(json_data, content_type='application/json')
 
-        json_data = JSONRenderer().render(serializer.errors)
-        return HttpResponse(json_data, content_type='application/json')
+#model object - single student's data
+def student_detail(request, pk):
+    stu = Student.objects.get(id=pk)  #complex data
+    serializer = StudentSerializer(stu)  # in python data
+    #json_data = JSONRenderer().render(serializer.data) # to convert python data into json
+    #return HttpResponse(json_data, content_type='application/json')
+    return JsonResponse(serializer.data)
+
+# query set - each student's data
+def student_list(request):
+    stu = Student.objects.all()  #complex data
+    serializer = StudentSerializer(stu, many=True)  # in python data
+    json_data = JSONRenderer().render(serializer.data) # to convert python data into json
+    return HttpResponse(json_data, content_type='application/json')
+    
