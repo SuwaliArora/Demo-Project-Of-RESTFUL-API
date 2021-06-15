4ade3f875eb3359101e8340ae0263ea369b8ca77
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-15 23:28:40 +0530

    Updated urls.py

--- NewProject2/NewProject2/urls.py
+++ NewProject2/NewProject2/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from NewProject2.api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('studentcreate/', views.student_create),
 ]
+
