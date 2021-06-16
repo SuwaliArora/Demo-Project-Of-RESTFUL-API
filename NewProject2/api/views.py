0c17db95be25e5de5ddf7611d48b1c268e943c94
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-16 23:36:31 +0530

    Updated views.py
    Import csrf to avoid an error.

--- NewProject2/api/views.py
+++ NewProject2/api/views.py
@@ -6,8 +6,10 @@ from rest_framework.parsers import JSONParser
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
+@csrf_exempt
 def student_create(request):
     if request.method == 'POST':
         json_data = request.body
