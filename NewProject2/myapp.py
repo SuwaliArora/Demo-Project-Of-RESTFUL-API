d39a88d7e69233cb7406b8397c8ca8c181401e9f
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-22 23:45:13 +0530

    Updated myapp.py
    To extract data in python datatypes and to create data.

--- NewProject2/myapp.py
+++ NewProject2/myapp.py
@@ -1,20 +1,33 @@
 import requests
 import json
 
-URL = "http://127.0.0.1:8000/studentcreate/"
+URL = "http://127.0.0.1:8000/studentdata/"
 
-data = {
-    'name' : 'Sonam',
-    'roll' : 101,
-    'city': 'Ranchi'
-}
-# to convert python data in json data
-json_data = json.dumps(data)
+def get_data(id = None):
+    data = {}
+    if id is not None:
+        data = {'id': id}
+    json_data = json.dumps(data)
+    r = requests.get(url = URL, data = json_data)
+    # to extract data from r 
+    data = r.json()
+    print(data)
 
-# response stored in r
-r = requests.post(url = URL, data = json_data)
+get_data()
 
-# to extract data from r 
-data = r.json()
+def post_data():
+    data = {
+        'name' : 'Sonam',
+        'roll' : 101,
+        'city': 'Ranchi'
+    }
+    # to convert python data in json data
+    json_data = json.dumps(data)
+    # response stored in r
+    r = requests.post(url = URL, data = json_data)
+    # to extract data from r 
+    data = r.json()
 
-print(data)
+    print(data)
+
+post_data()
