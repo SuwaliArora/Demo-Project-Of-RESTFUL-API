901db06604c33609f6ca3e161ba72f1636a84a40
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-22 23:52:21 +0530

    Updated urls.py

--- NewProject2/api/urls.py
+++ NewProject2/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studentcreate/', views.student_create),
+   # path('studentcreate/', views.student_create),
+    path('studentdata/', views.student_data),
 ]
