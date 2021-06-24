ddd51e6bb3161a45cbde518be52b250d0f9301b5
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-06-24 23:43:56 +0530

    Updated views.py
    Functions for the update, delete, get and post methods.

--- NewProject2/api/views.py
+++ NewProject2/api/views.py
@@ -1,3 +1,4 @@
+from functools import partial
 import json
 import re
 from django.shortcuts import render
@@ -5,11 +6,10 @@ import io
 from rest_framework.parsers import JSONParser
 from .serializers import StudentSerializer
 from rest_framework.renderers import JSONRenderer
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from .models import Student
-# Create your views here.
-   
+ 
 @csrf_exempt
 #view to extract serialized data as python data
 def student_data(request):
@@ -42,3 +42,34 @@ def student_data(request):
             return HttpResponse(json_data, content_type='application/json')
         json_data = JSONRenderer().render(serializer.errors)
         return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'PUT':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id') #id of the user whose data to be updated
+        stu = Student.objects.get(id=id)
+        #to convert python data into complex data
+        serializer = StudentSerializer(stu, data = pythondata, partial=True) #partial=true is for telling we have to update only partial data
+        # for complete data updation
+        #serializer = StudentSerializer(stu, data = pythondata)
+        if serializer.is_valid():
+            serializer.save()
+            res = {'msg': 'Data updated'}
+            json_data = JSONRenderer().render(res)
+            return HttpResponse(json_data, content_type='application/json')
+        json_data = JSONRenderer().render(serializer.errors)
+        return HttpResponse(json_data, content_type='application/json')
+
+    if request.method == 'DELETE':
+        json_data = request.body
+        stream = io.BytesIO(json_data)
+        pythondata = JSONParser().parse(stream)
+        id = pythondata.get('id') #id of the user whose data to be deleted
+        stu = Student.objects.get(id=id)
+        stu.delete()
+        res = {'msg': 'Data deleted'}
+        #json_data = JSONRenderer().render(res)
+        #return HttpResponse(json_data, content_type='application/json')
+        # inplace of above 2 lines of code
+        return JsonResponse(res, safe=True)
