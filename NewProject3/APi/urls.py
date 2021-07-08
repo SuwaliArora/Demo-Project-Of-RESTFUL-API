6fb7aaa509187fcfd7c95685e424b19fceae30b9
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-07-08 23:57:12 +0530

    Updated urls.py
    Implemented URL patterns for class-based APIView.

--- NewProject3/APi/urls.py
+++ NewProject3/APi/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('studentdata/', views.student_data),
-    path('studentdata/<int:pk>', views.student_data),
+    #path('studentdata/', views.student_data),
+    #path('studentdata/<int:pk>', views.student_data),
+    path('studentdata/', views.StudentAPI.as_view()),
+    path('studentdata/<int:pk>', views.StudentAPI.as_view()),
 ]
