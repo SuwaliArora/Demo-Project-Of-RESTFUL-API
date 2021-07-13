013954ad7c5a554111fa1a8b05d01b4d071ee340
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-07-13 17:23:26 +0530

    Updated views.py
    Learned and Implemented the concept of Concrete View Class and performed CRUD operations using it.
    
    Advantage of Concrete View Class: It helps to write small codes for CRUD operations as compared to code In projects "NewProject" and "NewProject2".

--- NewProject4/APi/views.py
+++ NewProject4/APi/views.py
@@ -1,4 +1,30 @@
-# GenericAPIView and Model Mixin
+#Concrete View Class
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import ListAPIView,  CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+
+class StudentList(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentCreate(CreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieve(RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentUpdate(UpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDestroy(DestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+
+'''# GenericAPIView and Model Mixin
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import GenericAPIView
@@ -28,3 +54,5 @@ class StudentRUD(GenericAPIView, RetrieveModelMixin, UpdateModelMixin, DestroyMo
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+'''
