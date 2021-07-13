639a7a3ed7b2ef13fb1a1495ae2c4556ed923c3b
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-07-13 17:26:41 +0530

    Updated urls.py
    Implemented new URL patterns for Concrete API View.
    
    Disadvantage of these Concrete API View Class: Only one URL pattern will work at time.

--- NewProject4/APi/urls.py
+++ NewProject4/APi/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
 from . import views
 
+'''
 urlpatterns = [
     path('studentdata/', views.StudentListCreate.as_view()),
     path('studentdata/<int:pk>', views.StudentRUD.as_view()),
+]
+'''
+urlpatterns = [
+    path('studentdata/', views.StudentList.as_view()),
+    path('studentdata/', views.StudentCreate.as_view()),
+    path('studentdata/<int:pk>', views.StudentRetrieve.as_view()),
+    path('studentdata/<int:pk>', views.StudentUpdate.as_view()),
+    path('studentdata/<int:pk>', views.StudentDestroy.as_view()),
 ]
