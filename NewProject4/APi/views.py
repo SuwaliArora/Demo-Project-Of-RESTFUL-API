38b7921a7198e2df801c2a2f294db8888d2eba1f
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-07-13 23:24:13 +0530

    Updated views.py
    Implemented ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView of Concrete API view class.

--- NewProject4/APi/views.py
+++ NewProject4/APi/views.py
@@ -2,6 +2,7 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView,  CreateAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateAPIView, RetrieveDestroyAPIView
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
@@ -23,6 +24,17 @@ class StudentDestroy(DestroyAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
+class StudentListCreate(ListCreateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveUpdate(RetrieveUpdateAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentRetrieveDestroy(RetrieveDestroyAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
 '''# GenericAPIView and Model Mixin
 from .models import Student
