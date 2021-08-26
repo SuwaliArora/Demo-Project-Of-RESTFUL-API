c8e5bd7d095524d086bd5e3247a1aca6eb92a4dc
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-08-26 21:04:09 +0530

    Update views.py
    Implemented custom permissions.

--- NewProject5/APi/views.py
+++ NewProject5/APi/views.py
@@ -7,7 +7,7 @@ from .serializers import StudentSerializer
 from rest_framework import status, viewsets]
 from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, DjangoModelPermissions, DjangoModelPermissionsOrAnonReadOnly
-
+from .custompermissions import MyPermission
 
 '''class StudentViewSet(viewsets.ViewSet):
     def list(self, request):
@@ -63,5 +63,6 @@ class StudentModelViewSet(viewsets.ModelViewSet):
     #permission_classes = [IsAuthenticated] #if user is authenticated then permissions allow to perform operations
     authentication_classes = [SessionAuthentication]
     #permission_classes = [IsAuthenticatedOrReadOnly]
-    permission_classes = [DjangoModelPermissions]
-    permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    #permission_classes = [DjangoModelPermissions]
+    #permission_classes = [DjangoModelPermissionsOrAnonReadOnly]
+    permission_classes = [MyPermission]
