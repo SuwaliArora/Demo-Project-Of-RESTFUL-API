92ecf8c0bbf04da80d16d23d8970da330976c744
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-08-27 14:11:43 +0530

    Updated urls.py
    Implemented token authentication by - 1. using Admin Application
                                                                    2. using Django manage.py command

--- NewProject5/APi/urls.py
+++ NewProject5/APi/urls.py
@@ -1,19 +1,18 @@
 from django.db import router
 from django.urls import path, include
 from . import views
-from rest_framework.routers import DefaultRouter
-# DefaultRouter includes a default API root view, that returns a 
+from rest_framework.routers import DefaultRouter # DefaultRouter includes a default API root view, that returns a 
 #response containing hyperlinks to all the list views
+from .auth import obtain_auth_token
 
 #creating Router Object 
 router = DefaultRouter()
 
 #register StudentViewSet with Router
-#router.register('studentdata', views.StudentViewSet, basename='student')
-
-#register StudentModelViewSet with Router
 router.register('studentdata', views.StudentModelViewSet, basename='student')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('gettoken/', obtain_auth_token)
 ]
