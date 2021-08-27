a9d565a5d474c81faa2bf2c73b66066e5e11b1e2
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-08-27 16:38:56 +0530

    Updated urls.py
    Implemented token authentication by 'exposing an API endpoint' in which the user will request the server for the token.

--- NewProject5/APi/urls.py
+++ NewProject5/APi/urls.py
@@ -3,7 +3,8 @@ from django.urls import path, include
 from . import views
 from rest_framework.routers import DefaultRouter # DefaultRouter includes a default API root view, that returns a 
 #response containing hyperlinks to all the list views
-from .auth import obtain_auth_token
+#from .auth import obtain_auth_token
+from .auth import CustomAuthToken
 
 #creating Router Object 
 router = DefaultRouter()
@@ -14,5 +15,6 @@ router.register('studentdata', views.StudentModelViewSet, basename='student')
 urlpatterns = [
     path('', include(router.urls)),
     path('auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('gettoken/', obtain_auth_token)
+    #path('gettoken/', obtain_auth_token)
+    path('gettoken/', CustomAuthToken.as_view())
 ]
