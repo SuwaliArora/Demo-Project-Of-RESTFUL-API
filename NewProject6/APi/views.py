746949245bde4c94a5f68cd7930b804a1fe437e9
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-06 23:57:52 +0530

    Update views.py
    Implemented the concept of throttling.

--- NewProject6/APi/views.py
+++ NewProject6/APi/views.py
@@ -1,12 +1,17 @@
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework import status, viewsets
-from rest_framework_simplejwt.authentication import JWTAuthentication
+#from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.throttling import AnonRateThrottle, UserRateThrottle
 
 # code to perform CRUD operations by implementing modelView
 class StudentModelViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    authentication_classes = [JWTAuthentication]
+    #authentication_classes = [JWTAuthentication]
+    #permission_classes = [IsAuthenticated]
+    authentication_classes = [SessionAuthentication]
     permission_classes = [IsAuthenticated]
+    throttle_classes = [AnonRateThrottle, UserRateThrottle]
