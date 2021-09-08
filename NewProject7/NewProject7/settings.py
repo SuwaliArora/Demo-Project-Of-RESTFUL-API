d9f2156b9bc571ec6c4badfc781a8e24faa57e48
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-08 22:11:59 +0530

    Updated settings.py
    Added global declaration for Django filters.

--- NewProject7/NewProject7/settings.py
+++ NewProject7/NewProject7/settings.py
@@ -126,3 +126,9 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# global declaration of using django-filters
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend']
+}
+
