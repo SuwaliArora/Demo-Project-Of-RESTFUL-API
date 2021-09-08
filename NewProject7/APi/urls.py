aea4129775b5ee0328e80d92c0dbda84edc606f9
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-08 21:58:20 +0530

    Updated urls.py

--- NewProject7/APi/urls.py
+++ NewProject7/APi/urls.py
@@ -1,14 +1,6 @@
 from django.urls import path
 from . import views
 
-'''urlpatterns = [
-    path('studentdata/', views.StudentListCreate.as_view()),
-    #path('studentdata/<int:pk>', views.StudentRetrieve.as_view()),
-    #path('studentdata/<int:pk>', views.StudentUpdate.as_view()),
-    path('studentdata/<int:pk>', views.StudentRUD.as_view()),
-]
-'''
 urlpatterns = [
-    #path('studentdata/', views.StudentListCreate.as_view()),
     path('studentdata/', views.StudentList.as_view()),
 ]
