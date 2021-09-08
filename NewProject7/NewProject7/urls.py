9587787cb1b47d810fdbd1717d34961cd6155f4c
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-08 21:52:47 +0530

    Add files via upload

---
+++ NewProject7/NewProject7/urls.py
@@ -0,0 +1,10 @@
+"""NewProject7 URL Configuration"""
+
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('APi.urls')),
+]
