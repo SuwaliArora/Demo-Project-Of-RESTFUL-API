58bdfe1a1a16aa27b12448d325e8d5e032c54f6a
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-09 17:10:36 +0530

    Updated views.py
    Implemented ordering filters(Order the data either in ascending or descending according to the attribute).

--- NewProject7/APi/views.py
+++ NewProject7/APi/views.py
@@ -7,8 +7,15 @@ from rest_framework.filters import SearchFilter
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    
+    filter_backends = [OrderingFilter]
+    #ordering_fields = ['city']    #order on the basis of city only
+    ordering_fields = ['name', 'city']    #ordering on the basis of city or name
+    #search_fields = ['^name']       # search on the basis of starting letter of name
+    
     #filterset_fields = ['name', 'city']
-    filter_backends = [SearchFilter]
-    search_fields = ['city']    #search on the basis of city only
+    
+    #filter_backends = [SearchFilter]
+    #search_fields = ['city']    #search on the basis of city only
     #search_fields = ['name', 'city']    #search on the basis of city or name
     #search_fields = ['^name']       # search on the basis of starting letter of name
