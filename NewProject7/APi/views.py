d2d2835b31539d417de46e389899ae5ce1345d94
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-09 22:44:32 +0530

    Update views.py
    For local declaration of PageNumberPagination, I have created a separate file pagination.py and imported in views.py

--- NewProject7/APi/views.py
+++ NewProject7/APi/views.py
@@ -3,19 +3,31 @@ from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
 from rest_framework.filters import SearchFilter
+from .pagination import PageNumberpagination
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = PageNumberpagination
     
+ ''' 
     filter_backends = [OrderingFilter]
     #ordering_fields = ['city']    #order on the basis of city only
     ordering_fields = ['name', 'city']    #ordering on the basis of city or name
     #search_fields = ['^name']       # search on the basis of starting letter of name
-    
-    #filterset_fields = ['name', 'city']
-    
+   
     #filter_backends = [SearchFilter]
     #search_fields = ['city']    #search on the basis of city only
     #search_fields = ['name', 'city']    #search on the basis of city or name
     #search_fields = ['^name']       # search on the basis of starting letter of name
+    
+    #filter_backends = [DjangoFilterBackend]
+    #filterset_fields = ['name', 'city']
+    
+    #filterset_fields = ['city']
+    # to view data of a particular user using passby attribute
+    #def get_queryset(self):
+     #   user = self.request.user
+     #   return Student.objects.filter(passby=user) 
+
+'''
