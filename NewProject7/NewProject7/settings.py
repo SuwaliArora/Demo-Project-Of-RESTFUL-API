9f6ea6c5396fca9fa56fc8f986cba1936da7538a
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-09 22:39:18 +0530

    Updated settings.py
    Globally declared PageNumberPagination in settings.py

--- NewProject7/NewProject7/settings.py
+++ NewProject7/NewProject7/settings.py
@@ -127,8 +127,10 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# global declaration of using django-filters
+# global declaration of using django-filters and Pagination
 REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE':5,   #number of records per page
     'DEFAULT_FILTER_BACKENDS':['django_filters.rest_framework.DjangoFilterBackend']
 }
 
