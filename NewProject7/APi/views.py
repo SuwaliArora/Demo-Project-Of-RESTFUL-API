48029127742fe0ef3dfcb00f807c1484478c23a3
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-09 17:05:55 +0530

    Updated views.py
    Implemented search filters.

--- NewProject7/APi/views.py
+++ NewProject7/APi/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 from .models import Student
 from .serializers import StudentSerializer
 from rest_framework.generics import ListAPIView
+from rest_framework.filters import SearchFilter
 
 class StudentList(ListAPIView):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-    filterset_fields = ['name', 'city']
+    #filterset_fields = ['name', 'city']
+    filter_backends = [SearchFilter]
+    search_fields = ['city']    #search on the basis of city only
+    #search_fields = ['name', 'city']    #search on the basis of city or name
+    #search_fields = ['^name']       # search on the basis of starting letter of name
