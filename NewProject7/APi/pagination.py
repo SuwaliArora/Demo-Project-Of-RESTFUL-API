6af4af4893483c0de9345040c45e238e170aa935
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-10 13:31:42 +0530

    Update pagination.py
    Local declaration of CursorPagination.
    
    While running the server, there was an error "FieldError" because we haven't used a parameter "created" in our models.py. So to remove that error we can make changes in the cursorpagination class and order the records by any attribute.

--- NewProject7/APi/pagination.py
+++ NewProject7/APi/pagination.py
@@ -1,8 +1,11 @@
 # locally define PageNumberPagination, LimitoffsetPagination
-from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination, CursorPagination
 
 class PageNumberpagination(PageNumberPagination):
     page_size = 5
     
 class LimitoffsetPagination(LimitOffsetPagination):
     default_limit = 5
+    
+class cursorpagination(CursorPagination):
+    page_size = 3
