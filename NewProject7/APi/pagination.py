6dcc0a70ade76e7f17cb7a13b3fe137d2727efaf
SuwaliArora <83538719+SuwaliArora@users.noreply.github.com>
2021-09-10 13:05:48 +0530

    Update pagination.py
    Declared locally LimitOffsetPagination(limit is used for how many records users want to see on a page and offset used for id users want to see the record).

--- NewProject7/APi/pagination.py
+++ NewProject7/APi/pagination.py
@@ -1,6 +1,8 @@
-# locally define PageNumberPagination
-from rest_framework.pagination import PageNumberPagination
+# locally define PageNumberPagination, LimitoffsetPagination
+from rest_framework.pagination import PageNumberPagination, LimitOffsetPagination
 
 class PageNumberpagination(PageNumberPagination):
     page_size = 5
     
+class LimitoffsetPagination(LimitOffsetPagination):
+    default_limit = 5
